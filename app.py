6004a41fa532d8e6a4b691c6a10170e09faf61ef
Meta-Baki <meteorolog138@gmail.com>
2026-08-22 03:36:34 +0400

    Update app.py

--- app.py
+++ app.py
@@ -536,4 +536,4 @@ def sitemap():
 # ---------------- RUN ----------------
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=False)
+    app.run(host="0.0.0.0", port=port, debug=False) 
